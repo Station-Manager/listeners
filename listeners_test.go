6bf33bc4348ed16869f9cd44a5c55e4c4cd139cd
ColonelBlimp <20266202+ColonelBlimp@users.noreply.github.com>
2026-03-18 10:26:27 +0200

    Refactor "listeners" tests: migrate to testify suite and improve assertions

---
+++ listeners_test.go
@@ -0,0 +1,58 @@
+package listeners
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/Station-Manager/config"
+	"github.com/Station-Manager/iocdi"
+	"github.com/Station-Manager/logging"
+	"github.com/Station-Manager/utils"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
+)
+
+type TestSuite struct {
+	suite.Suite
+}
+
+var container *iocdi.Container
+
+func (suite *TestSuite) SetupTest() {
+	container = iocdi.New()
+	err := container.Register(config.ServiceName, reflect.TypeOf(&config.Service{}))
+	assert.NoError(suite.T(), err)
+
+	err = container.RegisterInstance(logging.ServiceName, &logging.Service{})
+	assert.NoError(suite.T(), err)
+
+	err = container.Register(ServiceName, reflect.TypeOf(&Service{}))
+	assert.NoError(suite.T(), err)
+
+	workDir, err := utils.WorkingDir()
+	assert.NoError(suite.T(), err)
+
+	err = container.RegisterInstance("workingdir", workDir)
+	assert.NoError(suite.T(), err)
+
+	err = container.Build()
+	assert.NoError(suite.T(), err)
+}
+
+func TestListenerTestSuite(t *testing.T) {
+	suite.Run(t, new(TestSuite))
+}
+
+func (suite *TestSuite) TestInit() {
+	service, err := container.ResolveSafe(ServiceName)
+	assert.NoError(suite.T(), err)
+	assert.NotNil(suite.T(), service)
+}
+
+func (suite *TestSuite) TestStart() {
+	service, err := container.ResolveSafe(ServiceName)
+	assert.NoError(suite.T(), err)
+	assert.NotNil(suite.T(), service)
+	err = service.(*Service).Start()
+	assert.NoError(suite.T(), err)
+}
